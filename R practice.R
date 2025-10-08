bee06ce8b2092276be4f0d05d4cf31e7564d56cf
lucienpatchett <lucienpatchett@gmail.com>
2025-10-08 15:44:39 +0000

    updated mini-report with knitted .md file

---
+++ R practice.R
@@ -0,0 +1,21 @@
+#R practice
+?round() #pulls up up help
+round(x = 3.14159265) #define x, leave digits default
+round(x = 3.14159265,digits=3) #how many digits you want to use
+
+
+?c()
+
+
+?factor()
+
+
+?plot() #Generic X-Y Plotting option
+
+?read.csv
+data <- read.csv(file="bloodmeal_for_BIOL3070.csv")
+
+head(data) #gives you just the top part of the data
+View(data) #capital "V"
+str(data)
+dim(data) #dimenstions of data in one easy line to view
--- templateCode.R
+++ templateCode.R
@@ -1,1 +1,13 @@
 # Day 1 Example Plot
+
+viremia <- read.csv("viremia_data_full.csv")
+viremia
+view(viremia)
+colnames(viremia) <- c("Bird","n","Species","Family","Order","1","3","4","6")
+
+cols <- c("black","grey",rainbow(26)[4:26])
+
+plot(c(1,3,4,6),as.numeric(viremia[1,6:9]),
+     type = "1", 1wd =2, ylim  = range(viremia[,6:9],na.rm =TRUE),
+     xlab = "Day Postinfection"
+     ylab = "Log PFU/ml Serum")
